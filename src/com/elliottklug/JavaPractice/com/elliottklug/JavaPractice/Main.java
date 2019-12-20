2c2878d03209eb27f5cc6f3eefa839405afb8379
Elliott Klug <43073915+careeningspace@users.noreply.github.com>
2019-12-20 15:26:02 -0500

    Update for queue coding

--- src/com/elliottklug/JavaPractice/com/elliottklug/JavaPractice/Main.java
+++ src/com/elliottklug/JavaPractice/com/elliottklug/JavaPractice/Main.java
@@ -1,7 +1,5 @@
 package com.elliottklug.JavaPractice;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 /*        WebsiteClass websiteClass = new WebsiteClass();
@@ -35,11 +33,22 @@ public class Main {
 		System.out.println();
 		for (Product product : shipment.getLightVanProducts())
 			System.out.println(product.toString());
+		
+		HelpDesk helpDesk = new HelpDesk();
+		helpDesk.enquire(Customer.MARY, Category.PRINTER);
+		helpDesk.enquire(Customer.JACK, Category.PHONE);
+		helpDesk.enquire(Customer.JILL, Category.PRINTER);
+		helpDesk.enquire(Customer.MARY, Category.COMPUTER);
+		helpDesk.enquire(Customer.JACK, Category.PRINTER);
+
+		
+//		helpDesk.processAllEnquires();
+
+		helpDesk.processPrinterEnquiry();
+		helpDesk.processPrinterEnquiry();
+		helpDesk.processGeneralEnquiry();
+		helpDesk.processPrinterEnquiry();
     }
     
-	
-	static void AddItems() 
-	{
-	}
 
 }
--- src/com/elliottklug/JavaPractice/com/elliottklug/JavaPractice/Supplier.java
+++ src/com/elliottklug/JavaPractice/com/elliottklug/JavaPractice/Supplier.java
@@ -1,17 +1,31 @@
 package com.elliottklug.JavaPractice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Supplier {
+	
+	private String name;
+	private final List<Product> products = new ArrayList<>();
 
-	public Supplier(String string) {
-		// TODO Auto-generated constructor stub
+	public Supplier(String name) 
+	{
+		this.setName(name);
 	}
 
-	public List<Product> products() {
-		List<Product> product = null;
-		// TODO Auto-generated method stub
-		return product;
+	public List<Product> products() 
+	{
+		return products;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
 	}
 
 }
